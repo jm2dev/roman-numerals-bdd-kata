8d7225ea0c26d445fb930baa1e88bbd585325b54
jm2dev <jomarcar@gmail.com>
2012-11-28 23:28:20 +0000

    With table-driven property checks.

--- build.sbt
+++ build.sbt
@@ -7,7 +7,8 @@ version := "0.1.0-SNAPSHOT"
 scalaVersion := "2.10.0-RC2" 
 
 libraryDependencies ++= Seq(
-  "org.scalatest" % "scalatest_2.10.0-RC2" % "2.0.M5" % "test"
+  "org.scalatest" % "scalatest_2.10.0-RC2" % "2.0.M5" % "test",
+  "org.scalacheck" % "scalacheck_2.10.0-RC2" % "1.10.0" % "test"
 )
 
 initialCommands := "import com.jm2dev.kata.bddkata._"
--- src/test/scala/Decimal2RomanSpec.scala
+++ src/test/scala/Decimal2RomanSpec.scala
@@ -1,18 +1,36 @@
 package com.jm2dev.kata.bddkata
 
-import org.scalatest.FunSpec
+import org.scalatest.{GivenWhenThen, FunSpec}
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 
-class Decimal2RomanSpec extends FunSpec {
-  describe("Decimal to Roman numbers transformer") {
-   it("transforms 1 to I") {
-     val decimal = 1
-     val expected = "I"
-     val actual = new Decimal2Roman(decimal).transform
-     assert(actual == expected)
-   }
+class Decimal2RomanSpec extends FunSpec with GivenWhenThen with ShouldMatchers with TableDrivenPropertyChecks {
+  val numbers =
+    Table(
+      ("decimal", "roman"),
+      (1, "I"),
+      (2, "II"),
+      (3, "III")
+    )
+
+  describe("Convert decimal numbers to roman numerals") {
+    forAll (numbers) { (decimal: Int, roman: String) =>
+      Given("a decimal number equal to %s".format(decimal))
+      When("the roman numeral is generated")
+      val romanNumeral = DecimalNumber(decimal).toRoman
+      Then("roman numeral should be %s".format(roman))
+      romanNumeral should be (roman)
+
+      DecimalNumber(decimal).toRoman should be (roman)
+    }
   }
 }
 
-class Decimal2Roman(val decimal: Int) {
-  def transform: String = "I"
+case class DecimalNumber(val value: Int) {
+  def toRoman = value match {
+    case 1 => "I"
+    case 2 => "II"
+    case 3 => "III"
+    case _ => "Not a number"
+  }
 }
